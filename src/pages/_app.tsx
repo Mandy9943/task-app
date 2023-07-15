9695a35c2a74595b42ad1be6c629d7464cdbd9b6
Armando Cesar Martin Calderón <armandoc9943business@gmail.com>
2023-07-15 19:38:39 -0400

    add - custom them primary color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ConfigProvider } from "antd";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#6600b9",
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 }
